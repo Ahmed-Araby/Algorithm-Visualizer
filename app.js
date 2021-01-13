c939673e86ef476df7a097c45309fc229af029ef
Ahmed Araby <ahmedaraby605@gmail.com>
2021-01-13 07:44:08 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ function chooseAlgorithm()
 {
     var docObj = document.getElementById("algoCategory");
     algorithmCategory = docObj.options[docObj.selectedIndex].value;
-    
-    window.location.href = './' + algorithmCategory + '/index.html';
+    if(algorithmCategory == "SortingAlgorithms" ) // sorting algo 
+        window.location.href = './' + algorithmCategory + '/index.html';
+    else // convex hull
+        window.location.href = './' + algorithmCategory + '/ConvexHull/DivideAndConquer/index.html';
     return ;
 }
